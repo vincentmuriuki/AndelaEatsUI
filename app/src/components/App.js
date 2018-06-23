e64c900b1e39cbdb2acb8ec0a82b4433f18e6dfa
Charpell <charpellumeh@gmail.com>
2018-06-23 13:24:31 +0100

    chore(webpack): setup base project - setup project with weback 4 - create project folder structure - setup babel for transpiling - setup homepage component with react [Delivers #158386577]

---
+++ app/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Andela Eats Homepage</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
