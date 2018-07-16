64407c1adbaafadfe0a1eb4a3cf08be4589a401e
ibrahim013 <walaibrahim13@gmail.com>
2018-07-16 18:03:06 +0100

    feat(toastify):implemented toastify - added toastify - added coveralls [Delivers #159074060]

--- client/src/tests/components/Login.test.js
+++ client/src/tests/components/Login.test.js
@@ -8,17 +8,12 @@ expect
 */
 describe('Login Component', () => {
   const wrapper = shallow(<Login />);
-
   it('should render atleast once', () => {
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.length).toEqual(1);
   });
 
   it('renders a div', () => {
-    expect(wrapper.find('div').length).toBeGreaterThan(0);
-  });
-
-  it('always renders a form submit button ', () => {
-    expect(wrapper.find('.login-button').length).toEqual(1);
+    expect(wrapper.find('Redirect').length).toBeGreaterThan(0);
   });
 });
