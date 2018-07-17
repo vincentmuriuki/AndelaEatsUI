a8f95ebd5c41e26d806db446b264f7b465754b5b
Charpell <charpellumeh@gmail.com>
2018-07-17 09:28:47 +0100

    feat(dates): dynamically render dates - dynamically render dates on side bar  [Finishies #159069658]

--- client/src/components/Order/Menus.jsx
+++ client/src/components/Order/Menus.jsx
@@ -43,29 +43,32 @@ class Menus extends Component {
   updateSelection(mealCategory, mealId) {
     this.setState({ [mealCategory]: mealId });
   }
+  
 
   render () {
     const { match, data } = this.props;
   
     const menus = data.find(meals => meals.id === Number(match.params.id));
-    let mainMeal;
+    let mainMeal = [];
     let firstAccompaniment;
     let secondAccompaniment;
     if (menus) {
-      mainMeal = menus.meal.main || 'No Meal to Display';
-      firstAccompaniment = menus.meal.firstAccompaniment || 'No Meal to Display';
-      secondAccompaniment = menus.meal.secondAccompaniment || 'No Meal to Display';
+      mainMeal = menus.meal.main;
+      firstAccompaniment = menus.meal.firstAccompaniment;
+      secondAccompaniment = menus.meal.secondAccompaniment;
     }
 
+    
     return (
       <div>
         <div className="menus-container">
-          <MealOptions
-            category="mainMeal"
-            title="Main Meal"
-            mealOptions={mainMeal}
-            selectedMealId={this.state.mainMeal}
-            updateSelection={this.updateSelection}
+         { mainMeal.length > 0 ? <div>
+            <MealOptions
+              category="mainMeal"
+              title="Main Meal"
+              mealOptions={mainMeal}
+              selectedMealId={this.state.mainMeal}
+              updateSelection={this.updateSelection}
             />
             <MealOptions
               category="acc1"
@@ -80,14 +83,15 @@ class Menus extends Component {
               mealOptions={secondAccompaniment}
               updateSelection={this.updateSelection}
               selectedMealId={this.state.acc2}
-          />
-          <div className="cta">
-          <div className="float-left"></div>
-          <div className="float-right">
-            <div className="btn reset-order">reset order</div>
-            <div className="btn submit-order">submit order</div>
-          </div>
-          </div>
+            />
+            <div className="cta">
+            <div className="float-left"></div>
+            <div className="float-right">
+              <div className="btn reset-order">reset order</div>
+              <div className="btn submit-order">submit order</div>
+            </div>
+            </div>
+           </div> : <div>No meals have been posted yet</div>}
         </div>
       </div>
     )
