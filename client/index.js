35320140ab23ca36cacb973a2e3e89cffe1ad10e
Charpell <charpellumeh@gmail.com>
2018-12-10 18:25:19 +0100

    bg(staging): fix staging issues - refactor the server - remove dev server from webpack production
    [Delivers #162552064]

--- client/index.js
+++ client/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', express.static('dist'));
-app.use('*', express.static('dist'));
+app.use(express.static(`${__dirname}/dist`));
 
-app.listen(process.env.PORT || 8080);
+app.use('*', (request, response) => {
+  response.sendFile(`${__dirname}/src/index.html`);
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
